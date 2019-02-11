98ece8990bb0a5de94ed1334fbf98afac041d0e2
Hellthrashers <marcobenny_12@hotmail.com>
2019-02-10 22:04:06 -0600

    creacion de migracions con relaciones y fk error(rev)

--- database/migrations/2019_02_11_033904_add_images_table.php
+++ database/migrations/2019_02_11_033904_add_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email',30)->unique();
-            $table->string('password', 60);
-            $table->enum('type',['member','admin'])->default('member');
-            $table->rememberToken();
+            $table->unsignedInteger('article_id');
             $table->timestamps();
+
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade'); 
         });
-    }
+   }
 
     /**
      * Reverse the migrations.
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('images');
     }
 }
